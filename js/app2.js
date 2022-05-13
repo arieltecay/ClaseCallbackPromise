345e9126e0e20d5e82ea79b065ce61488b74fdde
arieltecay <arieltecay@gmail.com>
2022-05-13 10:40:31 -0300

    update

--- js/app2.js
+++ js/app2.js
@@ -1,19 +1,39 @@
 const aplicarDescuento = new Promise((resolve, reject) => {
-    const dto = false
+    const dto = true
     if (dto) {
         resolve('Descuento aplicado')
     } else {
         reject('Descuento no aplicado')
     }
 })
+/* Segundo metodo */
+const insertParagraph = (res) => {
+    const $h1 = document.querySelector("body > h1")
+    const $p = document.createElement("p");
+    $p.id = "p-message";
+    $p.textContent = `Se realizo ${res}`;
+    $h1.insertAdjacentElement("afterend", $p);
+}
+
 
 aplicarDescuento
     .then(res => {
-        console.log(res);
+        insertParagraph(res)
     })
     .catch(error => {
-        console.error(error);
+        insertParagraph(error);
+
     })
+
+
+/*
+Primer Metodo
+function mostrarHTML(texto) {
+    const aviso = document.querySelector("#promesa")
+    const parrafo = document.createElement('p')
+    parrafo.textContent = texto
+    aviso.appendChild(parrafo)
+} */
 
 /* Crear con Javascript un código para que se pueda mostrar en el DOM la info que devuelve la promesa */
 
